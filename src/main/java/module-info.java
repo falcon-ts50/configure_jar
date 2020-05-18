ad261b45442b499a4cfe5b7660addbe5ee3ded97
kirill <stroganov_k@mail.ru>
2020-05-18 19:48:05 +0300

    first working after rebase

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module configure_ups {
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires javafx.base;
+    requires javafx.fxml;
+    exports configure_UPS;
+    opens configure_UPS;
+}
