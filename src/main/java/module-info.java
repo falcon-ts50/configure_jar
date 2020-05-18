8e0591e5844cd683602566d9d8c373a514efafee
kirill <stroganov_k@mail.ru>
2020-05-19 00:04:31 +0300

    Fixing conflict version after rebase

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,6 @@ module configure_ups {
     requires javafx.controls;
     requires javafx.base;
     requires javafx.fxml;
-    exports configure_UPS;
-    opens configure_UPS;
+    exports configure_ups;
+    opens configure_ups;
 }
